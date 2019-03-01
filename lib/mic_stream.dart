5f94a5762b0c0d868433a5f013cc19c9a7d6d6b7
Aaron Alef <aaron.alef@code.berlin>
2019-03-01 17:27:20 +0100

    Fixed the stop function never returning

--- lib/mic_stream.dart
+++ lib/mic_stream.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
 import 'dart:typed_data';
+import 'dart:io';
 
 import 'package:simple_permissions/simple_permissions.dart';
 
 import 'package:flutter/services.dart';
+
+bool _isRecording = false;
+bool _isRunning = false;
 
 /* Class handling the microphone */
 class Microphone implements StreamController {
@@ -21,8 +25,6 @@ class Microphone implements StreamController {
   static const MIN_SAMPLE_RATE = 1;
   static const MAX_SAMPLE_RATE = 96000;
 
-  bool _isRecording = false;
-  bool _isRunning = false;
   int _bufferSize;
   DateTime _timestamp;
 
@@ -37,11 +39,12 @@ class Microphone implements StreamController {
 
   // Starts and returns an audio stream from the microphone, which is given back as Uint8List; each element has the length of .bufferSize
   // Throws an ArgumentError if Sample Rate is not between 1 and 16000
-  Future<Stream<Uint8List>> start(
+  Stream<Uint8List> start(
       {int audioSource = DEFAULT_AUDIO_SOURCE, int sampleRate = DEFAULT_SAMPLE_RATE, int channelConfig = DEFAULT_CHANNEL_CONFIG, int audioFormat = DEFAULT_AUDIO_FORMAT}
-      ) async {
+      ) async* {
     if (!_isRecording) {
       _isRecording = true;
+      _isRunning = true;
 
       print("Ask for permission to record audio...");
       await SimplePermissions.requestPermission(Permission.RecordAudio);
@@ -50,16 +53,16 @@ class Microphone implements StreamController {
       _timestamp = new DateTime.now();
 
       print("mic_stream: Set audio source");
-      await setAudioSource(audioSource);
+      setAudioSource(audioSource);
 
       print("mic_stream: Set sample rate");
-      await setSampleRate(sampleRate);
+      setSampleRate(sampleRate);
 
       print("mic_stream: Set channel config");
-      await setChannelConfig(channelConfig);
+      setChannelConfig(channelConfig);
 
       print("mic_stream: Set audio format");
-      await setAudioFormat(audioFormat);
+      setAudioFormat(audioFormat);
 
       print("mic_stream: Update buffer size");
       _bufferSize = await bufferSize;
@@ -73,10 +76,15 @@ class Microphone implements StreamController {
         throw(StateError);
       }
 
-      print("mic_stream: Start recording:");
-      _run();
+      print("mic_stream: Start writing bytes to buffer");
+      Duration _sleep = new Duration(milliseconds: 1);
+      while(_isRunning) {
+        if (_isRecording) yield await _platform.invokeMethod('getByteArray');
+        //sleep(_sleep);
+      }
 
-      return _controller.stream;
+      _platform.invokeMethod('releaseRecorder');
+      _controller.close();
     }
     else {
       throw("mic_stream: INITERROR - Microphone is already running!");
@@ -97,33 +105,24 @@ class Microphone implements StreamController {
     if (!_isRecording) {
       _isRecording = true;
       print("mic_stream: Resume recording");
-      _run();
     }
     else {
       print("mic_stream: Already recording");
     }
   }
 
-  Duration stop() {
-    _isRecording = false;
-    while (_isRunning) continue;
-    _platform.invokeMethod('releaseRecorder');
-    _controller.close();
-    return duration;
+  DateTime reset() {
+    pause();
+    DateTime old = _timestamp;
+    _timestamp = DateTime.now();
+    resume();
+    return old;
   }
 
-  // runs asynchronously in a loop and stores data to the stream
-  void _run() async {
-
-    print("mic_stream: Writing Bytes to buffer...");
-    _isRunning = true;
-    while(_isRecording) {
-      try {
-        _controller.add(await _platform.invokeMethod('getByteArray'));
-      } finally {}
-    }
-    print("mic_stream: Stop writing Bytes");
+  Duration _stop() {
     _isRunning = false;
+    _isRecording = false;
+    return duration;
   }
 
   // Changes the sample rate (only necessary for changing while recording - might cause unintended behaviour)
@@ -141,7 +140,7 @@ class Microphone implements StreamController {
   bool get isRecording => _isRecording;
 
   // Returns the duration since first start
-  Duration get duration =>_timestamp.difference(DateTime.now());
+  Duration get duration => -_timestamp.difference(DateTime.now());
 
   // Returns the amount of bytes per element (the length of one Uint8List)
   Future<int> get bufferSize async {
