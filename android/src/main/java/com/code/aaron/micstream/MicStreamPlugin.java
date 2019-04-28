c68319e238180b10bbb82e5d7b4bf40f17c0bafd
Aaron Alef <aaron.alef@code.berlin>
2019-04-28 16:14:03 +0200

    init project

--- android/src/main/java/com/code/aaron/micstream/MicStreamPlugin.java
+++ android/src/main/java/com/code/aaron/micstream/MicStreamPlugin.java
@@ -1,86 +1,84 @@
 package com.code.aaron.micstream;
 
-import android.media.AudioAttributes;
+import java.util.ArrayList;
+
 import android.media.AudioFormat;
 import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.Handler;
 
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** MicStreamPlugin
- *  In reference to https://stackoverflow.com/questions/33403656/stream-microphone-to-speakers-android
+ *  In reference to flutter sensors plugin
  */
-public class MicStreamPlugin implements MethodCallHandler {
+public class MicStreamPlugin implements EventChannel.StreamHandler {
+    private static final String MICROPHONE_CHANNEL_NAME = "aaron.code.com/mic_stream";
 
     /**
-     * Plugin registration.
+     * Plugin registration
      */
     public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "mic_stream");
-        channel.setMethodCallHandler(new MicStreamPlugin());
+        final EventChannel microphone = new EventChannel(registrar.messenger(), MICROPHONE_CHANNEL_NAME);
+        microphone.setStreamHandler(new MicStreamPlugin());
     }
 
-    /** Variables **/
-    private AudioRecord recorder;
-    private int AUDIO_SOURCE;
-    private int CHANNEL_CONFIG;
-    private int AUDIO_FORMAT;
-    private int SAMPLE_RATE;
-    private int BUFFER_SIZE;
-
-    public void onMethodCall(MethodCall call, Result result) {
-        switch (call.method) {
-            case "initRecorder":
-                BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT);
-                if (BUFFER_SIZE == AudioRecord.ERROR_BAD_VALUE) result.error("-1", "Bad buffer size value", null);
-
-                recorder = new AudioRecord(AUDIO_SOURCE, SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT, BUFFER_SIZE);
-                if (recorder.getState() == AudioRecord.STATE_UNINITIALIZED) result.error("-2", "Failed to initialize recorder", null);
-                recorder.startRecording();
-                break;
-
-            case "getPlatformVersion":
-                result.success("Android " + android.os.Build.VERSION.RELEASE);
-                return;
-
-            case "getBufferSize":
-                result.success(BUFFER_SIZE);
-                return;
-
-            case "getByteArray":
-                byte data[] = new byte[BUFFER_SIZE];
+    private final Handler handler = new Handler();
+    private final Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            isRunning = true;
+            while(isRunning) {
+                short[] data = new short[BUFFER_SIZE];
                 recorder.read(data, 0, BUFFER_SIZE);
-                result.success(data);
-                return;
-
-            case "setSampleRate":
-                SAMPLE_RATE = call.argument("sampleRate");
-                break;
-
-            case "setAudioSource":
-                AUDIO_SOURCE = call.argument("audioSource");
-                break;
-
-            case "setChannelConfig":
-                CHANNEL_CONFIG = call.argument("channelConfig");
-                break;
-
-            case "setAudioFormat":
-                AUDIO_FORMAT = call.argument("audioFormat");
-                break;
-
-            case "releaseRecorder":
-                recorder.stop();
-                recorder.release();
-                break;
-
-            default:
-                result.notImplemented();
-                return;
+                eventSink.success(data);
+            }
         }
-        result.success("Success");
+    };
+
+    private boolean isRunning = false;
+    private int AUDIO_SOURCE = MediaRecorder.AudioSource.DEFAULT;
+    private int SAMPLE_RATE = 16000;
+    private int CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO;
+    private int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
+    private int BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT);
+
+    private EventChannel.EventSink eventSink;
+    private AudioRecord recorder;
+
+    @Override
+    public void onListen(Object args, final EventChannel.EventSink eventSink) {
+        ArrayList<Integer> config = (ArrayList<Integer>) args;
+
+        // Set parameters, if available
+        switch(config.size()) {
+            case 4:
+                AUDIO_FORMAT = config.get(3);
+            case 3:
+                CHANNEL_CONFIG = config.get(2);
+            case 2:
+                SAMPLE_RATE = config.get(1);
+            case 1:
+                AUDIO_SOURCE = config.get(0);
+            default:
+                BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT);
+        }
+
+        this.eventSink = eventSink;
+        recorder = new AudioRecord(AUDIO_SOURCE, SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT, BUFFER_SIZE);
+        if (recorder.getState() != AudioRecord.STATE_INITIALIZED) eventSink.error("-1", "PlatformError", null);
+        recorder.startRecording();
+        if (recorder.getRecordingState() != AudioRecord.RECORDSTATE_RECORDING) eventSink.error("-2", "PlatformError", null);
+        runnable.run();
+    }
+
+    @Override
+    public void onCancel(Object o) {
+        isRunning = false;
+        eventSink.endOfStream();
+        handler.removeCallbacks(runnable);
+        recorder.release();
+        recorder = null;
     }
 }
