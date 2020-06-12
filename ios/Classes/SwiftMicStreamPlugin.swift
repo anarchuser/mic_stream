f7819c3c2e3e42ea42eea16614ac7451e046ee70
Nick Fisher <nick.fisher@avinium.com>
2020-06-11 23:21:29 -0700

    add swift plugin

--- ios/Classes/SwiftMicStreamPlugin.swift
+++ ios/Classes/SwiftMicStreamPlugin.swift
@@ -1,135 +1,151 @@
 import Flutter
 import UIKit
+import AVFoundation
+import Dispatch
 
-public class SwiftMicStreamPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterEventChannel(name:"aaron.code.com/mic_stream", binaryMessenger: registrar.messenger())
-    channel.setStreamHandler(SwiftMicStreamPlugin());
-      }
+enum AudioFormat : Int { case ENCODING_PCM_8BIT, ENCODING_PCM_16BIT }
+enum ChannelConfig : Int { case CHANNEL_IN_MONO	, CHANNEL_IN_STEREO }
+enum AudioSource : Int { case DEFAULT }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+public class SwiftMicStreamPlugin: NSObject, FlutterStreamHandler, FlutterPlugin, AVCaptureAudioDataOutputSampleBufferDelegate {
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterEventChannel(name:"aaron.code.com/mic_stream", binaryMessenger: registrar.messenger())
+        channel.setStreamHandler(SwiftMicStreamPlugin());
+    }
 
-public class SwiftAudioStreamingRecorderPlugin: NSObject, FlutterPlugin, AVAudioRecorderDelegate {
-  
-  let isRecording:Bool = false;
-  let AUDIO_FORMAT:Int = 0;
-  let CHANNEL_CONFIG:Int = 0;
-  let SAMPLE_RATE:Int = 0;
-  let AUDIO_SOURCE = 0;
-  let BUFFER_SIZE =0;
+    let isRecording:Bool = false;
+    var AUDIO_FORMAT:AudioFormat = AudioFormat.ENCODING_PCM_16BIT;
+    var CHANNEL_CONFIG:ChannelConfig = ChannelConfig.CHANNEL_IN_MONO;
+    var SAMPLE_RATE:Int = 0;
+    var AUDIO_SOURCE:AudioSource = AudioSource.DEFAULT;
+    var BUFFER_SIZE = 0;
 
-  var session : AVCaptureSession!
+    var session : AVCaptureSession!
 
-  public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {    
+    public func onCancel(withArguments arguments:Any?) -> FlutterError?  {
+        return nil
+    }
 
-    if (isRecording) return;
-
-        let config = args as? [Int!}];
+    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        return nil
+        if (isRecording) {
+            return nil;
+        }
+    
+        let config = arguments as! [Int?];
 
         // Set parameters, if available
         switch config.count {
             case 4:
-                AUDIO_FORMAT = config.get(3);
+                AUDIO_FORMAT = AudioFormat(rawValue:config[3]!)!;
             case 3:
-                CHANNEL_CONFIG = config.get(2);
+                CHANNEL_CONFIG = ChannelConfig(rawValue:config[2]!)!;
             case 2:
-                SAMPLE_RATE = config.get(1);
+                SAMPLE_RATE = config[1]!;
             case 1:
-                AUDIO_SOURCE = config.get(0);
+                AUDIO_SOURCE = AudioSource(rawValue:config[0]!)!;
+                //if(AUDIO_SOURCE != 0)
+                //    events(FlutterError(code: "-3",
+                //                 message: "Currently only default AUDIO_SOURCE (id: 0) is supported"))
+                //return nil
+                    //              details: e)) // in case
             default:
-              do {
-                try {
-                    BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT);
-                } catch (Exception e) {
-                   events(FlutterError(code: "-3",
-                             message: "Invalid AudioRecord parameters",
-                             details: e)) // in case o
-                }
+              //do {
+                BUFFER_SIZE = 1024; //try AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT);
+              //} catch let e {
+               //    events(FlutterError(code: "-3",
+               //              message: "Invalid AudioRecord parameters",
+               //              details: e)) // in case
+              //}
+            // this.eventSink = new MainThreadEventSink(eventSink);
+
+            // // Try to initialize and start the recorder
+            // recorder = new AudioRecord(AUDIO_SOURCE, SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT, BUFFER_SIZE);
+            // if (recorder.getState() != AudioRecord.STATE_INITIALIZED) eventSink.error("-1", "PlatformError", null);
+            // recorder.startRecording();
         }
-
-        // this.eventSink = new MainThreadEventSink(eventSink);
-
-        // // Try to initialize and start the recorder
-        // recorder = new AudioRecord(AUDIO_SOURCE, SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT, BUFFER_SIZE);
-        // if (recorder.getState() != AudioRecord.STATE_INITIALIZED) eventSink.error("-1", "PlatformError", null);
-        // recorder.startRecording();                  
-
-
-         if let audioCaptureDevice : AVCaptureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeAudio) {
+        startCapture();
+        return nil;
+    }
+    
+    func startCapture() {
+    
+        if let audioCaptureDevice : AVCaptureDevice = AVCaptureDevice.default(for:AVMediaType.audio) {
 
             self.session = AVCaptureSession()
+            do {
+                try audioCaptureDevice.lockForConfiguration()
+                
+                let audioInput = try AVCaptureDeviceInput(device: audioCaptureDevice)
+                audioCaptureDevice.unlockForConfiguration()
 
-            try audioCaptureDevice.lockForConfiguration()
+                if(self.session.canAddInput(audioInput)){
+                    self.session.addInput(audioInput)
+                }
+                
+                try AVAudioSession.sharedInstance().setPreferredSampleRate(try Double(SAMPLE_RATE))
+                try AVAudioSession.sharedInstance().setPreferredInputNumberOfChannels(CHANNEL_CONFIG.rawValue + 1)
+                try AVAudioSession.sharedInstance().setPreferredOutputNumberOfChannels(CHANNEL_CONFIG.rawValue + 1)
+                let audioOutput = AVCaptureAudioDataOutput()
+                
+              //  audioOutput.audioSettings = [
+                  
+                   // AVLinearPCMBitDepthKey: AUDIO_FORMAT == AudioFormat.ENCODING_PCM_8BIT ? 8 : 16
+//];
+                audioOutput.setSampleBufferDelegate(self, queue: DispatchQueue.global())
 
-            let audioInput = try AVCaptureDeviceInput(device: audioCaptureDevice)
-            audioCaptureDevice.unlockForConfiguration()
+                if(self.session.canAddOutput(audioOutput)){
+                    self.session.addOutput(audioOutput)
+                }
 
-            if(captureSession.canAddInput(audioInput)){
-                captureSession.addInput(audioInput)
-            }
-
-            let audioOutput = AVCaptureAudioDataOutput()
-
-            audioOutput.setSampleBufferDelegate(self, queue: GlobalUserInitiatedQueue)
-
-            if(captureSession.canAddOutput(audioOutput)){
-                captureSession.addOutput(audioOutput)
-            }
-
-            dispatch_async(GlobalUserInitiatedQueue) {
-                self.captureSession.startRunning()
+                //supposed to start session not on UI queue coz it takes a while
+                DispatchQueue.main.async {
+                    print("starting captureSession")
+                    self.session.startRunning()
+                }
+            } catch let e {
+                print(e)
             }
         }
-        return nil                                                                                                                                                                           
-    }                              
-  
-
-func captureOutput(_            output      : AVCaptureOutput,
+    }
+    
+    public func captureOutput(_            output      : AVCaptureOutput,
                    didOutput    sampleBuffer: CMSampleBuffer,
                    from         connection  : AVCaptureConnection) {
 
-    var buffer: CMBlockBuffer? = nil
+        var buffer: CMBlockBuffer? = nil
 
-    // Needs to be initialized somehow, even if we take only the address
-    let convenianceBuffer = AudioBuffer(mNumberChannels: 1, mDataByteSize: 0, mData: nil)
-    var audioBufferList = AudioBufferList(mNumberBuffers: 1,
+        // Needs to be initialized somehow, even if we take only the address
+        let convenianceBuffer = AudioBuffer(mNumberChannels: 1, mDataByteSize: 0, mData: nil)
+        var audioBufferList = AudioBufferList(mNumberBuffers: 1,
                                           mBuffers: convenianceBuffer)
 
-    CMSampleBufferGetAudioBufferListWithRetainedBlockBuffer(
-        sampleBuffer,
-        bufferListSizeNeededOut: nil,
-        bufferListOut: &audioBufferList,
-        bufferListSize: MemoryLayout<AudioBufferList>.size(ofValue: audioBufferList),
-        blockBufferAllocator: nil,
-        blockBufferMemoryAllocator: nil,
-        flags: UInt32(kCMSampleBufferFlag_AudioBufferList_Assure16ByteAlignment),
-        blockBufferOut: &buffer
-    )
+        CMSampleBufferGetAudioBufferListWithRetainedBlockBuffer(
+            sampleBuffer,
+            bufferListSizeNeededOut: nil,
+            bufferListOut: &audioBufferList,
+            bufferListSize: MemoryLayout<AudioBufferList>.size(ofValue: audioBufferList),
+            blockBufferAllocator: nil,
+            blockBufferMemoryAllocator: nil,
+            flags: UInt32(kCMSampleBufferFlag_AudioBufferList_Assure16ByteAlignment),
+            blockBufferOut: &buffer
+        )
 
-    let abl = UnsafeMutableAudioBufferListPointer(&audioBufferList)
+        let abl = UnsafeMutableAudioBufferListPointer(&audioBufferList)
 
-    for buffer in abl {
-        let originRawPtr = buffer.mData
-        let ptrDataSize = Int(buffer.mDataByteSize)
+        for buffer in abl {
+            let originRawPtr = buffer.mData
+            let ptrDataSize = Int(buffer.mDataByteSize)
 
-        // From raw pointer to typed Int16 pointer
-        let buffPtrInt16 = originRawPtr?.bindMemory(to: Int16.self, capacity: ptrDataSize)
+            // From raw pointer to typed Int16 pointer
+            let buffPtrInt16 = originRawPtr?.bindMemory(to: Int16.self, capacity: ptrDataSize)
 
-        // From pointer typed Int16 to pointer of [Int16]
-        // So we can iterate on it simply
-        let unsafePtrByteSize = ptrDataSize/Int16.bitWidth
-        let samples = UnsafeMutableBufferPointer<Int16>(start: buffPtrInt16,
+            // From pointer typed Int16 to pointer of [Int16]
+                // So we can iterate on it simply
+            let unsafePtrByteSize = ptrDataSize/Int16.bitWidth
+            let samples = UnsafeMutableBufferPointer<Int16>(start: buffPtrInt16,
                                                         count: unsafePtrByteSize)
-
-        // Average of each sample squared, then root squared
-        let sumOfSquaredSamples = samples.map(Float.init).reduce(0) { $0 + $1*$1 }
-        let averageOfSomething = sqrt(sumOfSquaredSamples / Float(samples.count))
-
-        DispatchQueue.main.async {
-            print("Calulcus of something: \(String(averageOfSomething))" )
+            print("got buffer")
         }
     }
-}
 }
