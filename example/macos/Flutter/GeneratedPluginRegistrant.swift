b0abbf073ed8b4f0a791b1914d8933351df7aa5e
xloc <xloc.cc@outlook.com>
2022-12-13 01:31:50 -0800

    change macOS native implementation: AVAudioEngine (#50)
    Resolves #49

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import mic_stream
-import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MicStreamPlugin.register(with: registry.registrar(forPlugin: "MicStreamPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
