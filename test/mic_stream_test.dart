1a9309f2c2c4e5e9d9e4861eb98524c8e6f3defd
Aaron Alef <aaron.alef@code.berlin>
2023-10-02 18:38:25 +0200

    docs: udpate docs for version 0.7.1

--- test/mic_stream_test.dart
+++ test/mic_stream_test.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/services.dart';
-import 'package:mic_stream/mic_stream.dart';
+// import 'package:flutter/services.dart';
+// import 'package:mic_stream/mic_stream.dart';
 
 void main() {
 }
